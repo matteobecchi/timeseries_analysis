51ee293e0539120c0786cecbfeb71bca215f9307
Matteo Becchi <bechmath@gmail.com>
2023-07-19 16:21:09 +0200

    Commenting the code

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import scipy.optimize
-# import scipy.stats
-# import pycwt as wavelet
 from functions import *
 
 ### System specific parameters ###
@@ -12,11 +10,13 @@ t_units = r'[ns]'			# Units of measure of time
 
 ### Usually no need to changhe these ###
 output_file = 'states_output.txt'
-show_plot = False
+show_plot = True
 
 def all_the_input_stuff():
+	# Read input parameters from files.
 	data_directory, PAR = read_input_parameters()
 
+	# Read raw data from the specified directory/files.
 	if type(data_directory) == str:
 		M_raw = read_data(data_directory)
 	else:
@@ -24,24 +24,36 @@ def all_the_input_stuff():
 		M1 = read_data(data_directory[1])
 		M_raw = np.array([ np.concatenate((M0[i], M1[i])) for i in range(len(M0)) ])
 
+	# Remove initial frames based on 'tau_delay'.
 	M_raw = M_raw[:, PAR[1]:]
+
+	# Apply Savitzky-Golay filtering if 'tau_window' > 3, otherwise, keep the raw data.
 	if PAR[0] > 3:
 		M = Savgol_filter(M_raw, PAR[0])
 	else:
 		M = np.copy(M_raw)
 		print('\tWARNING: no data smoothing. ')
-	M_raw = None ### Freeing memory ###
 
+	# Normalize the data to the range [0, 1].
 	SIG_MAX = np.max(M)
 	SIG_MIN = np.min(M)
 	M = (M - SIG_MIN)/(SIG_MAX - SIG_MIN)
 
+	# Get the number of particles and total frames in the trajectory.
 	total_particles = M.shape[0]
 	total_time = M.shape[1]
+
+	# Calculate the number of windows for the analysis.
+	num_windows = int(total_time / PAR[0])
+
+	# Print informative messages about trajectory details.
 	print('\tTrajectory has ' + str(total_particles) + ' particles. ')
 	print('\tTrajectory of length ' + str(total_time) + ' frames (' + str(total_time*PAR[2]) + ' ns). ')
-	print('\tUsing ' + str(int(total_time/PAR[0])) + ' windows of length ' + str(PAR[0]) + ' frames (' + str(PAR[0]*PAR[2]) + ' ns). ')
-	all_the_labels = np.zeros((len(M), int(total_time/PAR[0])))
+	print('\tUsing ' + str(num_windows) + ' windows of length ' + str(PAR[0]) + ' frames (' + str(PAR[0]*PAR[2]) + ' ns). ')
+
+	# Initialize an array to store labels for each window.
+	all_the_labels = np.zeros((M.shape[0], num_windows))
+	 # Initialize an empty list to store unique states in each window.
 	list_of_states = []
 
 	### Create files for output
@@ -50,6 +62,7 @@ def all_the_input_stuff():
 	if not os.path.exists('output_figures'):
 		os.makedirs('output_figures')
 
+	# Return required data for further analysis.
 	return M, PAR, data_directory, all_the_labels, list_of_states
 
 def plot_input_data(M, PAR, filename):
@@ -57,21 +70,31 @@ def plot_input_data(M, PAR, filename):
 	tau_delay = PAR[1]
 	t_conv = PAR[2]
 
+	# Flatten the M matrix and compute histogram counts and bins
 	flat_M = M.flatten()
 	counts, bins = np.histogram(flat_M, bins='auto', density=True)
 	counts *= flat_M.size
 
+	# Create a plot with two subplots (side-by-side)
 	fig, ax = plt.subplots(1, 2, sharey=True, gridspec_kw={'width_ratios': [3, 1]}, figsize=(9, 4.8))
+
+	# Plot histogram in the second subplot (right side)
 	ax[1].stairs(counts, bins, fill=True, orientation='horizontal', alpha=0.5)
 
+	# Compute the time array for the x-axis of the first subplot (left side)
 	time = np.linspace(tau_delay + int(tau_window/2), tau_delay + int(tau_window/2) + M.shape[1], M.shape[1])*t_conv
+
+	# Plot the individual trajectories in the first subplot (left side)
+    # If there are more than 1000 frames, plot only every 10th frame for faster rendering
 	if M.shape[1] > 1000:
 		for mol in M[::10]:
-			ax[0].plot(time, mol, c='xkcd:black', ms=0.1, lw=0.1, alpha=0.5, rasterized=True)
+			ax[0].plot(time, mol, c='xkcd:black', lw=0.1, alpha=0.5, rasterized=True)
 	else:
 		for mol in M:
-			ax[0].plot(time, mol, c='xkcd:black', ms=0.1, lw=0.1, alpha=0.5, rasterized=True)
+			ax[0].plot(time, mol, c='xkcd:black', lw=0.1, alpha=0.5, rasterized=True)
 
+	# Set labels and titles for the plots
+	ax[0].set_ylim([0.0, 1.0])
 	ax[0].set_ylabel('Normalized signal')
 	ax[0].set_xlabel(r'Simulation time $t$ ' + t_units)
 	ax[1].set_xticklabels([])
@@ -93,8 +116,8 @@ def gauss_fit_max(M, filename):
 		gap = 3
 
 	### 2. Smoothing with tau = 3 ###
-	counts = moving_average(counts, 3)
-	bins = moving_average(bins, 3)
+	counts = moving_average(counts, gap)
+	bins = moving_average(bins, gap)
 
 	### 3. Find the maximum ###
 	max_val = counts.max()
@@ -110,7 +133,7 @@ def gauss_fit_max(M, filename):
 
 	### 5. Try the fit between the minima and check its goodness ###
 	flag_min = 1
-	goodness_min = 4
+	goodness_min = 5
 	Bins = bins[min_id0:min_id1]
 	Counts = counts[min_id0:min_id1]
 	mu0 = bins[max_ind]
@@ -133,6 +156,8 @@ def gauss_fit_max(M, filename):
 		for j in range(len(perr)):
 			if perr[j]/popt_min[j] > 0.5:
 				goodness_min -= 1
+		if min_id1 - min_id0 <= gap:
+			goodness_min -= 1
 	except RuntimeError:
 		flag_min = 0
 	except TypeError:
@@ -150,7 +175,7 @@ def gauss_fit_max(M, filename):
 
 	### 7. Try the fit between the minima and check its goodness ###
 	flag_half = 1
-	goodness_half = 4
+	goodness_half = 5
 	Bins = bins[half_id0:half_id1]
 	Counts = counts[half_id0:half_id1]
 	mu0 = bins[max_ind]
@@ -173,6 +198,8 @@ def gauss_fit_max(M, filename):
 		for j in range(len(perr)):
 			if perr[j]/popt_half[j] > 0.5:
 				goodness_half -= 1
+		if min_id1 - min_id0 < gap:
+			goodness_half -= 1
 	except RuntimeError:
 		flag_half = 0
 	except TypeError:
@@ -322,7 +349,6 @@ def plot_cumulative_figure(M, PAR, list_of_states, final_list, data_directory, f
 	ax[0].set_ylim(y_lim)
 	ax[1].set_xticklabels([])
 
-	plt.show()
 	if show_plot:
 		plt.show()
 	fig.savefig(filename + '.png', dpi=600)
@@ -339,6 +365,7 @@ def plot_one_trajectory(M, PAR, all_the_labels, filename):
 	times = np.linspace(t_lim[0], t_lim[1], M.shape[1])
 	signal = M[example_ID]
 	color = all_the_labels[example_ID]
+	ax.plot(times, signal, c='black', lw=0.1)
 	ax.scatter(times, signal, c=color, vmin=0, vmax=np.max(np.unique(all_the_labels)), s=1.0)
 
 	fig.suptitle('Example particle: ID = ' + str(example_ID))
@@ -391,50 +418,77 @@ def plot_all_trajectory_with_histos(M, PAR, filename):
 
 def sankey(all_the_labels, frame_list, aver_window, t_conv, filename):
 	print('* Computing and plotting the averaged Sankey diagrams...')
+
+	# Check if the required frame range is within the bounds of the input data.
 	if frame_list[-1] + aver_window > all_the_labels.shape[1]:
 		print('\tERROR: the required frame range is out of bound.')
 		return
 
+	# Determine the number of unique states in the data.
 	n_states = np.unique(all_the_labels).size
-	source = np.empty((frame_list.size - 1)*n_states**2)
-	target = np.empty((frame_list.size - 1)*n_states**2)
-	value = np.empty((frame_list.size - 1)*n_states**2)
+
+	# Create arrays to store the source, target, and value data for the Sankey diagram.
+	source = np.empty((frame_list.size - 1) * n_states**2)
+	target = np.empty((frame_list.size - 1) * n_states**2)
+	value = np.empty((frame_list.size - 1) * n_states**2)
+
+	# Initialize a counter variable.
 	c = 0
+
+	# Create temporary lists to store node labels for the Sankey diagram.
 	tmp_label1 = []
 	tmp_label2 = []
 
+	# Loop through the frame_list and calculate the transition matrix for each time window.
 	for i, t0 in enumerate(frame_list[:-1]):
+		# Calculate the time jump for the current time window.
 		t_jump = frame_list[i + 1] - frame_list[i]
+
+		# Initialize a matrix to store the transition counts between states.
 		T = np.zeros((n_states, n_states))
+	    
+		# Iterate through the current time window and increment the transition counts in T.
 		for t in range(t0, t0 + aver_window):
 			for L in all_the_labels:
 				T[int(L[t])][int(L[t + t_jump])] += 1
 
+		# Store the source, target, and value for the Sankey diagram based on T.
 		for n1 in range(len(T)):
 			for n2 in range(len(T[n1])):
-				source[c] = n1 + i*n_states
-				target[c] = n2 + (i + 1)*n_states
+				source[c] = n1 + i * n_states
+				target[c] = n2 + (i + 1) * n_states
 				value[c] = T[n1][n2]
 				c += 1
 
+		# Calculate the starting and ending fractions for each state and store node labels.
 		for n in range(n_states):
-			starting_fraction = np.sum(T[n])/np.sum(T)
-			ending_fraction = np.sum(T.T[n])/np.sum(T)
+			starting_fraction = np.sum(T[n]) / np.sum(T)
+			ending_fraction = np.sum(T.T[n]) / np.sum(T)
 			if i == 0:
-				tmp_label1.append('State ' + str(n) + ': ' + "{:.2f}".format(starting_fraction*100) + '%')
-			tmp_label2.append('State ' + str(n) + ': ' + "{:.2f}".format(ending_fraction*100) + '%')
+				tmp_label1.append('State ' + str(n) + ': ' + "{:.2f}".format(starting_fraction * 100) + '%')
+			tmp_label2.append('State ' + str(n) + ': ' + "{:.2f}".format(ending_fraction * 100) + '%')
 
+	# Concatenate the temporary labels to create the final node labels.
 	label = np.concatenate((tmp_label1, np.array(tmp_label2).flatten()))
-	palette = sns.color_palette('viridis', n_colors=n_states-2).as_hex()
+
+	# Generate a color palette for the Sankey diagram.
+	palette = sns.color_palette('viridis', n_colors=n_states - 2).as_hex()
 	palette.insert(0, '#440154')
 	palette.append('#fde725')
+
+	# Tile the color palette to match the number of frames.
 	color = np.tile(palette, frame_list.size)
 
+	# Create dictionaries to define the Sankey diagram nodes and links.
 	node = dict(label=label, pad=30, thickness=20, color=color)
 	link = dict(source=source, target=target, value=value)
+
+	# Create the Sankey diagram using Plotly.
 	Data = go.Sankey(link=link, node=node, arrangement="perpendicular")
 	fig = go.Figure(Data)
-	fig.update_layout(title='Frames: ' + str(frame_list*t_conv) + ' ns')
+
+	# Add the title with the time information.
+	fig.update_layout(title='Frames: ' + str(frame_list * t_conv) + ' ns')
 
 	if show_plot:
 		fig.show()
@@ -451,20 +505,11 @@ def transition_matrix(Delta, t_conv, all_the_labels, filename):
 			id1 = int(mol[t + Delta])
 			T[id0][id1] += 1.0
 
-	N = np.zeros((n_states, n_states))
-	for i, row in enumerate(T):
-		if np.sum(row) > 0:
-			for j, el in enumerate(row):
-				N[i][j] = row[j]/np.sum(row)
-
-	N_min = np.max(N)
-	for (i, j), val in np.ndenumerate(N):
-		if val < N_min and val > 0.0:
-			N_min = val
+	N = T / T.sum(axis=1, keepdims=True)
 
 	fig, ax = plt.subplots(figsize=(10, 8))
-	im = ax.imshow(N, cmap='viridis', norm=LogNorm(vmin=N_min, vmax=np.max(N)))
-	fig.colorbar(im)
+	im = ax.imshow(N, cmap='viridis', norm=LogNorm(vmin=N[N > 0].min(), vmax=N.max()))
+	fig.colorbar(im, ax=ax, format='%.2f')
 	for (i, j), val in np.ndenumerate(N):
 		ax.text(j, i, "{:.2f}".format(val), ha='center', va='center')
 	fig.suptitle(r'Transition probabilities, $\Delta t=$' + str(Delta*t_conv) + ' ' + t_units)
@@ -472,11 +517,12 @@ def transition_matrix(Delta, t_conv, all_the_labels, filename):
 	ax.set_ylabel('From...')
 	ax.xaxis.tick_top()
 	ax.xaxis.set_label_position('top')
-	ax.set_xticks(np.linspace(0.0, n_states - 1.0, n_states))
+	ax.set_xticks(np.arange(n_states))
+	ax.set_yticks(np.arange(n_states))
 	ax.set_xticklabels(range(n_states))
-	ax.set_yticks(np.linspace(0.0, n_states - 1.0, n_states))
 	ax.set_yticklabels(range(n_states))
-	
+	plt.tight_layout()
+
 	if show_plot:
 		plt.show()
 	fig.savefig(filename + '.png', dpi=600)
@@ -714,18 +760,18 @@ def main():
 	all_the_labels = assign_final_states_to_single_frames(M, final_list)
 
 	plot_cumulative_figure(M, PAR, list_of_states, final_list, data_directory, 'output_figures/Fig2')
-	# # plot_all_trajectory_with_histos(M, PAR, 'output_figures/Fig2a')
+	# plot_all_trajectory_with_histos(M, PAR, 'output_figures/Fig2a')
 	plot_one_trajectory(M, PAR, all_the_labels, 'output_figures/Fig3')
 
-	# print_mol_labels_fbf_gro(all_the_labels, PAR, 'all_cluster_IDs.dat')
-	# print_mol_labels_fbf_xyz(all_the_labels, PAR, 'all_cluster_IDs_xyz.dat')
+	print_mol_labels_fbf_gro(all_the_labels, 'all_cluster_IDs.dat')
+	print_mol_labels_fbf_xyz(all_the_labels, 'all_cluster_IDs_xyz.dat')
 
-	# for i, frame_list in enumerate([np.array([0, 1]), np.array([0, 100, 200, 300, 400])]):
-	# 	sankey(all_the_labels, frame_list, 10, PAR[2], 'output_figures/Fig4_' + str(i))
+	for i, frame_list in enumerate([np.array([0, 1]), np.array([0, 100, 200, 300, 400])]):
+		sankey(all_the_labels, frame_list, 10, PAR[2], 'output_figures/Fig4_' + str(i))
 
-	# transition_matrix(1, PAR[2], all_the_labels, 'output_figures/Fig5')
-	# state_statistics(M, PAR, all_the_labels, 'output_figures/Fig6')
-	# # transition_statistics(M, PAR, all_the_labels, list_of_states, 'output_figures/Fig7')
+	transition_matrix(1, PAR[2], all_the_labels, 'output_figures/Fig5')
+	state_statistics(M, PAR, all_the_labels, 'output_figures/Fig6')
+	# transition_statistics(M, PAR, all_the_labels, list_of_states, 'output_figures/Fig7')
 
 if __name__ == "__main__":
 	main()
