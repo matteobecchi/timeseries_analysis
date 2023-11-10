9f27a0ee0bd162bbc36e6452ebe5001a70da42fc
Matteo Becchi <bechmath@gmail.com>
2023-11-10 16:53:28 +0100

    Solved many problems in 2D clustering

--- 2D-main.py
+++ 2D-main.py
@@ -576,7 +576,7 @@ def TRA_analysis(M_raw, PAR, perform_anew):
 
 def main():
 	M_raw, PAR = all_the_input_stuff()
-	# TRA_analysis(M_raw, PAR, True)
+	TRA_analysis(M_raw, PAR, True)
 	full_output_analysis(M_raw, PAR)
 
 if __name__ == "__main__":
