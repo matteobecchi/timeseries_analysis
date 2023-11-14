0c8a76b448015c13058635ea6b46f48f1774a990
Matteo Becchi <bechmath@gmail.com>
2023-11-14 12:16:52 +0100

    Stable version november 2023

--- functions.py
+++ functions.py
@@ -15,51 +15,6 @@ import plotly.graph_objects as go
 import plotly.io as pio
 import plotly.express as px
 
-class State:
-	def __init__(self, mu, sigma, A):
-		number_of_sigmas = 2.0 							# The amplitude of the fluctiations INSIDE a state
-		self.mu = mu 									# Mean of the Gaussian
-		self.sigma = sigma 								# Variance of the Gaussian
-		self.A = A 										# Area below the Gaussian
-		self.peak = A/sigma/np.sqrt(np.pi)				# Height of the Gaussian peak
-		self.perc = 0 									# Fraction of data points classified in this state
-		self.th_inf = [mu - number_of_sigmas*sigma, -1]	# Lower thrashold of the state
-		self.th_sup = [mu + number_of_sigmas*sigma, -1]	# Upper thrashold of the state
-
-class State_multi_D:
-	def __init__(self, mu, sigma, A):
-		number_of_sigmas = 2.0 				# The amplitude of the fluctiations INSIDE a state
-		self.mu = mu 						# Mean of the Gaussians
-		self.sigma = sigma 					# Variance of the Gaussians
-		self.A = A 							# Area below the Gaussians
-		self.perc = 0 						# Fraction of data points classified in this state
-		self.a = number_of_sigmas*sigma		# Axes of the state
-
-class Parameters:
-	def __init__(self, input_file):
-		try:
-			with open(input_file, 'r') as file:
-				lines = file.readlines()
-				param = [line.strip() for line in lines]
-		except:
-			print('\tinput_parameters.txt file missing or wrongly formatted.')
-		if len(param) < 6:
-			print('\tinput_parameters.txt file wrongly formatted.')
-
-		self.tau_w = int(param[0])
-		self.t_smooth = int(param[1])
-		self.t_delay = int(param[2])
-		self.t_conv = float(param[3])
-		self.t_units = r'[' + str(param[4]) + r']'
-		self.example_ID = int(param[5])
-		self.bins = 'auto'
-
-		if len(param) == 7:
-			print('\tWARNING: overriding histogram binning')
-			self.bins = int(param[6])
-		elif len(param) > 7:
-			print('\tinput_parameters.txt file wrongly formatted.')
-
 def read_input_data():
 	# Step 1: Attempt to read the content of 'data_directory.txt' file and load it into a NumPy array as strings.
 	try:
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from classes import *
 from functions import *
 
 output_file = 'states_output.txt'
@@ -589,7 +590,7 @@ def full_output_analysis(M_raw, PAR):
 	plot_one_trajectory(M, PAR, all_the_labels, 'Fig3')
 
 	print_mol_labels_fbf_xyz(all_the_labels)
-	# print_colored_trj_from_xyz('OW.xyz', all_the_labels, PAR)
+	# print_colored_trj_from_xyz('trajectory.xyz', all_the_labels, PAR)
 
 	# for i, frame_list in enumerate([np.array([0, 1]), np.array([0, 100, 200])]):
 	# 	sankey(all_the_labels, frame_list, 10, PAR[3], 'Fig4_' + str(i))
