44aa716154f2c62f121a93f73d740b36be2e9d77
Matteo Becchi <bechmath@gmail.com>
2023-11-15 12:08:59 +0100

    Aver sequence figure added in multivariate clustering

--- 2D-main.py
+++ 2D-main.py
@@ -544,6 +544,46 @@ def timeseries_analysis(M_raw, PAR):
 	else:
 		return len(list_of_states), fraction_0
 
+def compute_cluster_mean_seq(M, all_the_labels, tau_window):
+	if M.shape[2] > 2:
+		return
+
+	# Initialize lists to store cluster means and standard deviations
+	center_list = []
+	std_list = []
+
+	# Loop through unique labels (clusters)
+	for L in np.unique(all_the_labels):
+		tmp = []
+		# Iterate through molecules and their labels
+		for i, mol in enumerate(all_the_labels):
+			for w, l in enumerate(mol):
+				 # Define time interval
+				t0 = w*tau_window
+				t1 = (w + 1)*tau_window
+				# If the label matches the current cluster, append the corresponding data to tmp
+				if l == L:
+					tmp.append(M[i][t0:t1])
+	
+		# Calculate mean and standard deviation for the current cluster
+		center_list.append(np.mean(tmp, axis=0))
+		# std_list.append(np.std(tmp, axis=0))
+
+	# Plotting
+	fig, ax = plt.subplots()
+	for l, center in enumerate(center_list):
+		x = center[:, 0]
+		y = center[:, 1]
+		ax.plot(x, y, label='ENV'+str(l), marker='o')
+	fig.suptitle('Average time sequence inside each environments')
+	ax.set_xlabel(r'Signal 1')
+	ax.set_ylabel(r'Signal 2')
+	ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+	ax.legend()
+
+	plt.show()
+	fig.savefig('output_figures/Fig4.png', dpi=600)
+
 def full_output_analysis(M_raw, PAR):
 	tau_w = PAR.tau_w
 	M, all_the_labels = preparing_the_data(M_raw, PAR)
@@ -553,8 +593,9 @@ def full_output_analysis(M_raw, PAR):
 	if len(list_of_states) == 0:
 		print('* No possible classification was found. ')
 		return
-	all_the_labels = assign_single_frames(all_the_labels, tau_w)
 
+	compute_cluster_mean_seq(M, all_the_labels, tau_w)
+	all_the_labels = assign_single_frames(all_the_labels, tau_w)
 	plot_cumulative_figure(M, PAR, all_the_labels, list_of_states, 'Fig2')
 
 	print_mol_labels_fbf_xyz(all_the_labels)
