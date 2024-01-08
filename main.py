4c25c4359a6d5633377e4063b3a649e903c80708
Matteo Becchi <bechmath@gmail.com>
2024-01-08 16:38:05 +0100

    Bug with external states fixed

--- main.py
+++ main.py
@@ -692,7 +692,7 @@ def main():
     full_output_analysis() performs a detailed analysis with the chosen parameters.
     """
     data, par = all_the_input_stuff()
-    time_resolution_analysis(data, par, False)
+    time_resolution_analysis(data, par, True)
     full_output_analysis(data, par)
 
 if __name__ == "__main__":
