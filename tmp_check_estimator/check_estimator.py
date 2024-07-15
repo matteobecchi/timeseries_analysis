ba263a1b1e1fcb40b507de619ce76a84e601620a
Matteo Becchi <bechmath@gmail.com>
2024-07-15 17:01:49 +0200

    Solving compatibility issues with scikit in OnionMulti

--- tmp_check_estimator/check_estimator.py
+++ tmp_check_estimator/check_estimator.py
@@ -19,7 +19,6 @@ def run_the_checks_1():
         try:
             function_name = getattr(check[1], "__name__", str(check[1]))
             check[1](OnionUni())
-            print(f"{function_name}: PASSED")
         except Exception as e:
             print(f"{function_name}: FAILED\n{e}")
 
@@ -37,5 +36,5 @@ def run_the_checks_2():
             print(f"{function_name}: FAILED\n{e}")
 
 
-# run_the_checks_1()
-run_the_checks_2()
+run_the_checks_1()
+# run_the_checks_2()
