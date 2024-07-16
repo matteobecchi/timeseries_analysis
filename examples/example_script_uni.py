2686a13a4ce4fda8021e3f13c72a13c6021118b3
Matteo Becchi <bechmath@gmail.com>
2024-07-16 15:18:10 +0200

    improved example_uni formatting

--- examples/example_script_uni.py
+++ examples/example_script_uni.py
@@ -1,5 +1,5 @@
 """
-Example script for running onion_clustering
+Example script for running onion_uni
 """
 
 import matplotlib.pyplot as plt
@@ -22,39 +22,43 @@ from onion_clustering.onion_uni import onion_uni
 PATH_TO_INPUT_DATA = "onion_example_files/data/univariate_time-series.npy"
 
 ### Clustering at one specific value of time resolution ###
-N_WINDOWS = 99
+TAU_WINDOW = 5
 
 input_data = np.load(PATH_TO_INPUT_DATA)[:, 1:]
 n_particles = input_data.shape[0]
 n_frames = input_data.shape[1]
 
-tau_window = int(n_frames / N_WINDOWS)
-frames_in_excess = n_frames - N_WINDOWS * tau_window
+n_windows = int(n_frames / TAU_WINDOW)
+frames_in_excess = n_frames - n_windows * TAU_WINDOW
 reshaped_data = np.reshape(
-    input_data[:, :-frames_in_excess], (n_particles * N_WINDOWS, tau_window)
+    input_data[:, :-frames_in_excess], (n_particles * n_windows, TAU_WINDOW)
 )
 
 state_list, labels = onion_uni(reshaped_data)
 
 ### These functions are examples of how to visualize the results
-plot_output_uni("Fig1.png", reshaped_data, N_WINDOWS, state_list)
-plot_one_trj_uni("Fig2.png", 1234, reshaped_data, labels, N_WINDOWS)
+plot_output_uni("Fig1.png", reshaped_data, n_windows, state_list)
+plot_one_trj_uni("Fig2.png", 1234, reshaped_data, labels, n_windows)
 plot_medoids_uni("Fig3.png", reshaped_data, labels)
-plot_state_populations("Fig4.png", N_WINDOWS, labels)
-plot_sankey("Fig5.png", labels, N_WINDOWS, [10, 20, 30, 40])
+plot_state_populations("Fig4.png", n_windows, labels)
+plot_sankey("Fig5.png", labels, n_windows, [10, 20, 30, 40])
 
 ### Clustering at all the possible time resolution ###
-TMP_LIST = np.geomspace(2, 249, num=20, dtype=int)
-N_WINDOWS_LIST = [x for i, x in enumerate(TMP_LIST) if x not in TMP_LIST[:i]]
+TMP_LIST = np.geomspace(2, 499, num=20, dtype=int)
+TAU_WINDOWS = [x for i, x in enumerate(TMP_LIST) if x not in TMP_LIST[:i]]
 
-tra = np.zeros((len(N_WINDOWS_LIST), 3))
+tra = np.zeros((len(TAU_WINDOWS), 3))
 list_of_pop = []
 
-for i, n_windows in enumerate(N_WINDOWS_LIST):
-    tau_window = int(n_frames / n_windows)
+for i, tau_window in enumerate(TAU_WINDOWS):
+    n_windows = int(n_frames / tau_window)
     frames_in_excess = n_frames - n_windows * tau_window
+    if frames_in_excess > 0:
+        tmp_input_data = input_data[:, :-frames_in_excess]
+    else:
+        tmp_input_data = input_data
     reshaped_data = np.reshape(
-        input_data[:, :-frames_in_excess],
+        tmp_input_data,
         (n_particles * n_windows, tau_window),
     )
 
@@ -70,6 +74,6 @@ for i, n_windows in enumerate(N_WINDOWS_LIST):
 
 ### These functions are examples of how to visualize the results
 plot_time_res_analysis("Fig6.png", tra)
-plot_pop_fractions("Fig7.png", list_of_pop[::-1])
+plot_pop_fractions("Fig7.png", list_of_pop)
 
 plt.show()
